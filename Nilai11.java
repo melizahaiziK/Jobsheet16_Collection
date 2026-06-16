bb4cda3e906faaf511c0f5684ba3c1b3c5822042
melizahaiziK <melizaha1zik@gmail.com>
2026-06-16 23:06:46 +0700

    tugas 1

---
+++ Nilai11.java
@@ -0,0 +1,13 @@
+public class Nilai11 {
+    Mahasiswa11 mahasiswa;
+    Matakuliah11 mataKuliah;
+    double nilai;
+
+    public Nilai11(Mahasiswa11 mahasiswa, Matakuliah11 mataKuliah,
+                 double nilai) {
+
+        this.mahasiswa = mahasiswa;
+        this.mataKuliah = mataKuliah;
+        this.nilai = nilai;
+    }
+}
